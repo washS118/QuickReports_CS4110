e9259cb3f7ce24375f18ce24b0bee94dacd18b4d
Lucas <lukegauldin93@gmail.com>
2019-11-19 15:11:02 -0500

    Added basic fragment manager code.

--- app/src/main/java/com/quickreports/MainActivity.java
+++ app/src/main/java/com/quickreports/MainActivity.java
@@ -1,14 +1,35 @@
 package com.quickreports;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        SetFragment(new RecordListView());
+    }
+
+    public void LoadRecordListView(){
+        SetFragment(new RecordListView());
+    }
+
+    public void LoadRecordEditView(){
+        SetFragment(new RecordEditView());
+    }
+
+    private void SetFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/quickreports/RecordEditView.java
@@ -0,0 +1,86 @@
+package com.quickreports;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RecordEditView.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link RecordEditView#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RecordEditView extends Fragment {
+    private OnFragmentInteractionListener mListener;
+
+    public RecordEditView() {
+        // Required empty public constructor
+    }
+
+    public static RecordEditView newInstance() {
+        RecordEditView fragment = new RecordEditView();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_record_edit_view, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    private void LoadRecordListView(){
+        ((MainActivity)getActivity()).LoadRecordListView();
+    }
+}

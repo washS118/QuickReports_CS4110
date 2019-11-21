36b178580b0ded05bb471ea1c7d999c97d9234ef
Lucas <lukegauldin93@gmail.com>
2019-11-21 18:25:55 -0500

    Almost Finished Camera

--- app/src/main/java/com/quickreports/RecordEditView.java
+++ app/src/main/java/com/quickreports/RecordEditView.java
@@ -153,11 +153,7 @@ public class RecordEditView extends Fragment {
             }
         });
 
-<<<<<<< Updated upstream
         Log.println(Log.ERROR, LogTag, "Start Request");
-        weather.GetWeatherData(location);
-=======
-        Log.println(Log.ERROR, "Edit", "Start Request");
         weather.GetWeatherData(location);
 >>>>>>> Stashed changes
 

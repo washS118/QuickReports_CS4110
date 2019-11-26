2521a58e279c4cd7a6bf90505e3dc94705d75d52
Austin Hansford <austinhnsfrd@gmail.com>
2019-11-26 17:39:39 -0500

    More DB stuff

--- app/src/main/java/com/quickreports/MainActivity.java
+++ app/src/main/java/com/quickreports/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.net.Uri;
 
+import android.database.Cursor;
 import android.net.Uri;
 
 import android.database.Cursor;

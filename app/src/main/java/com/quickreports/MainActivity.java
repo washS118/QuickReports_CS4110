ab7e394b4bfde3fc74c4dcd61f2304a695db66ca
Lucas <lukegauldin93@gmail.com>
2019-11-26 16:05:21 -0500

    Fixed fragment switching code

--- app/src/main/java/com/quickreports/MainActivity.java
+++ app/src/main/java/com/quickreports/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.quickreports.Managers.DatabaseManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecordEditView.OnFragmentInteractionListener, RecordListView.OnFragmentInteractionListener {
+    private static String LogTag = "QuickReports-MainActivity";
     FragmentManager fragmentManager;
     Button btnCreateReport;
     Button viewReports;
@@ -27,28 +30,6 @@ public class MainActivity extends AppCompatActivity {
         reportDB = new DatabaseManager(this);
     }
 
-    public void addReportData() {
-        btnCreateReport.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //reportDB.addReport(reportTitle.getText(), reportDescription.getText(), getTimeInString(), getPathOfPhoto());
-                    }
-                }
-        );
-    }
-
-    public void viewReportData() {
-        btnCreateReport.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //reportDB.addReport(reportTitle.getText(), reportDescription.getText(), getTimeInString(), getPathOfPhoto());
-                    }
-                }
-        );
-    }
-
     public void LoadRecordListView(){
         SetFragment(new RecordListView());
     }
@@ -58,8 +39,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void SetFragment(Fragment fragment){
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, fragment);
-        fragmentTransaction.commit();
+        try {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        } catch (Exception e) {
+            Log.println(Log.ERROR, LogTag, "Set Fragment Error\n" + e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }

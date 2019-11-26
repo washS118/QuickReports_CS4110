f1e0895c2f70023cf00543c229be8ddcad920503
Lucas <lukegauldin93@gmail.com>
2019-11-26 16:39:44 -0500

    Fixed Weather Manager

--- app/src/main/java/com/quickreports/Managers/ApiService.java
+++ app/src/main/java/com/quickreports/Managers/ApiService.java
@@ -7,9 +7,8 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 interface ApiService {
-
-    @GET("api.openweathermap.org/data/2.5/weather?lat=<<lat>>&lon=<<lon>>&APPID=<<key>>")
+    @GET("weather")
     Single<Model> GetWeatherData(@Query("lat") double lat,
                                  @Query("lon") double lon,
-                                 @Query("key") String apiKey);
+                                 @Query("appid") String appid);
 }
